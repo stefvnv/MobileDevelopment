23232c70a91779eef4026799c26db34e9f8f45a4
stefvnv <stefvnv@yahoo.com>
2022-01-26 17:30:17 +0000

    screen1-almost3

--- Worksheet1/app/src/main/java/com/example/worksheet1/Screen2Activity.java
+++ Worksheet1/app/src/main/java/com/example/worksheet1/Screen2Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Screen2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_screen2);
     }
 }
