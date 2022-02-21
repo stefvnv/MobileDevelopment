cb113d4ddff87ac16f360253cde6347237570fd5
stefvnv <stefvnv@yahoo.com>
2022-02-21 17:59:20 +0000

    app1 - started greetings

--- App1/app/src/main/java/com/example/app1/GreetingsActivity.java
+++ App1/app/src/main/java/com/example/app1/GreetingsActivity.java
@@ -2,7 +2,9 @@ package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class GreetingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class GreetingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greetings);
+
+        //Sounds
+        ImageButton imgHello = findViewById(R.id.imgHello);
+
+        imgHello.setOnClickListener(View -> {
+            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.hola);
+            mp.start();
+        });
+
+
+        ImageButton imgHowAreYou = findViewById(R.id.imgHowAreYou);
+
+        imgHowAreYou.setOnClickListener(View -> {
+            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.howareyou);
+            mp.start();
+        });
+
+
+
     }
 }
