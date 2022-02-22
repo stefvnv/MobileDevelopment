c860633db17d2c93009efd7b7311cb7b64772295
stefvnv <stefvnv@yahoo.com>
2022-02-22 00:02:27 +0000

    Conversation updated - not complete

--- App1/app/src/main/java/com/example/app1/AboutActivity.java
+++ App1/app/src/main/java/com/example/app1/AboutActivity.java
@@ -5,15 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
-
-    private RadioGroup radioGroup;
-    private RadioButton radioCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +15,10 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
     }
 
-    //Radio Groups and buttons
+    //Radio Group
     public void goCity(View v) {
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         int selectedId = radioGroup.getCheckedRadioButtonId();
-        radioCity = (RadioButton) findViewById(selectedId);
 
         if (selectedId == R.id.barcelona) {
             Intent i = new Intent(AboutActivity.this, BarcelonaActivity.class);
--- App1/app/src/main/java/com/example/app1/ConversationActivity.java
+++ App1/app/src/main/java/com/example/app1/ConversationActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class GreetingsActivity extends AppCompatActivity {
+public class ConversationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,88 +18,91 @@ public class GreetingsActivity extends AppCompatActivity {
         //Sounds
         ImageButton imgHello = findViewById(R.id.imgHello);
         imgHello.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.hola);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.hola);
             mp.start();
         });
 
         ImageButton imgGoodMorning = findViewById(R.id.imgGoodMorning);
         imgGoodMorning.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.goodmorning);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.goodmorning);
             mp.start();
         });
 
         ImageButton imgGoodAfternoon = findViewById(R.id.imgGoodAfternoon);
         imgGoodAfternoon.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.goodafternoon);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.goodafternoon);
             mp.start();
         });
 
-
         ImageButton imgGoodnight = findViewById(R.id.imgGoodNight);
         imgGoodnight.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.goodnight);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.goodnight);
             mp.start();
         });
 
         ImageButton imgHowAreYou = findViewById(R.id.imgHowAreYou);
         imgHowAreYou.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.howareyou);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.howareyou);
             mp.start();
         });
 
         ImageButton imgGood = findViewById(R.id.imgGood);
         imgGood.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.good);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.good);
             mp.start();
         });
 
         ImageButton imgWhatsYourName = findViewById(R.id.imgWhatsYourName);
         imgWhatsYourName.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.whatsyourname);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.whatsyourname);
             mp.start();
         });
 
         ImageButton imgMyName = findViewById(R.id.imgMyName);
         imgMyName.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.mynameis);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.mynameis);
             mp.start();
         });
 
         ImageButton imgPleased = findViewById(R.id.imgPleased);
         imgPleased.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.pleased);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.pleased);
             mp.start();
         });
 
         ImageButton imgHowOldAreYou = findViewById(R.id.imgHowOldAreYou);
         imgHowOldAreYou.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.howold);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.howold);
             mp.start();
         });
 
         ImageButton imgIamYearsOld = findViewById(R.id.imgIamYearsOld);
         imgIamYearsOld.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.iamyearsold);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.iamyearsold);
             mp.start();
         });
 
         ImageButton imgSeeYouLater = findViewById(R.id.imgSeeYouLater);
         imgSeeYouLater.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.seeyou);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.seeyou);
             mp.start();
         });
 
         ImageButton imgGoodbye = findViewById(R.id.imgGoodbye);
         imgGoodbye.setOnClickListener(View -> {
-            MediaPlayer mp = MediaPlayer.create(GreetingsActivity.this, R.raw.goodbye);
+            MediaPlayer mp = MediaPlayer.create(ConversationActivity.this, R.raw.goodbye);
             mp.start();
         });
     }
 
     //Easy Button Click
-    public void goNextActivity(View view) {
-        Intent i = new Intent(GreetingsActivity.this, ConversationActivity.class);
+    public void goPreviousActivity(View view) {
+        Intent i = new Intent(ConversationActivity.this, GreetingsActivity.class);
         startActivity(i);
     }
 
+    public void goNextActivity(View view) {
+        Intent i = new Intent(ConversationActivity.this, ConversationActivity.class);
+        startActivity(i);
+    }
 }
